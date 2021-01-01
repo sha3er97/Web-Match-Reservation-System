d195ea6bb8f50edac8eb778ebfb38dcefafb74f4
MennaFekry <mennafekry98@gmail.com>
2021-01-01 11:35:49 -0800

    adding migrations

---
+++ BackEnd/webampp/database/migrations/2021_01_01_180707_create_matches_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+   
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->bigIncrements('id'); // primary key
+            $table->bigInteger('home')->references('number')->on('teams')->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('away')->references('number')->on('teams')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('stadium')->references('name')->on('stadia')->onUpdate('cascade')->onDelete('cascade');
+            $table->date('match_date');
+            $table->dateTime('match_time');
+            $table->string('main_referee');
+            $table->string('lineman_1');
+            $table->string('lineman_2');
+
+            $table->timestamps();
+
+            // restrictions
+            //$table->foreign('stadium')->references('name')->on('stadia')->onUpdate('cascade')->onDelete('cascade');
+            //$table->foreign('home')->references('number')->on('teams')->onUpdate('cascade')->onDelete('cascade');
+            //$table->foreign('away')->references('number')->on('teams')->onUpdate('cascade')->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
