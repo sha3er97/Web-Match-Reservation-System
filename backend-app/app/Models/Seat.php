64e074feb21bb6f6f11c779d7f47152bd51307ba
Reham Gamal <47679181+RehamGamal97@users.noreply.github.com>
2021-04-03 17:35:25 +0200

    match, seat, stadium models and controllers

--- backend-app/app/Models/Seat.php
+++ backend-app/app/Models/Seat.php
@@ -8,4 +8,67 @@ use Illuminate\Database\Eloquent\Model;
 class Seat extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'row',
+        'column',
+        'state',
+        'stadium',
+        'user',
+        'match',
+
+    ];
+
+
+     /*
+    funcitonality: reserve seat in a match
+
+    parameter: all table attributes
+
+    response: database row of reserved seat 
+     */
+
+    public function reserve ($id, $row, $column, $state, $stadium, $user, $match) {
+        return DB::table('seats')->insert([
+            'username' => $id,
+            'password' => $row,
+            'firstname' => $column,
+            'lastname' => $state,
+            'birthdate' => $stadium,
+            'gender' => $user,
+            'city' => $match,
+   
+
+        ]);
+    }
+
+     /*
+    funcitonality: get all seats of match
+
+    parameter: match id
+
+    response: list of database of all reserved seats
+     */
+
+    
+    public function getseats ($matchid) {
+        $seatss = DB::table('seats')->ehere('match',$matchid)->get();
+
+        return $seatss;
+    }
+
+    /*
+    funcitonality:delete reserved seat from match
+
+    parameter: seat id
+
+    response: delete seat from databes
+     */
+
+    public function cancelReservation ($seatID) {
+        return DB::table('seats')->where('id', $seatID)->delete();
+    }
+
+
 }
--- backend-app/app/Models/Stadium.php
+++ backend-app/app/Models/Stadium.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Stadium extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'number_of_rows', 'number_of_columns'
+    ];
+
+
+
+         /*
+    funcitonality: add  new stadium
+
+    parameter: all table attributes
+
+    response: database row of that user 
+     */
+
+    public function AddStadium ($name, $number_of_rows, $number_of_columns) {
+        return DB::table('users')->insert([
+            'name' => $name,
+            'number_of_rows' => $number_of_rows,
+            'number_of_columns' => $number_of_columns,
+        
+        ]);
+    }
+
 }
