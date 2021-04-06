b03183c888dce4375659ecfbd4d4b45be80cc8ff
sha3er97 <sha3er97@gmail.com>
2021-04-06 14:27:21 +0200

    [update] minor fixes in login/signup forms

--- FrontEnd/scripts/signup.js
+++ FrontEnd/scripts/signup.js
@@ -1,21 +1,33 @@
-import { sendHttpRequest } from './requestUtility.js'
+import {sendHttpRequest} from './requestUtility.js'
+
 $(document).ready(function () {
     $("#myBtn").click(function () {
-        url = "https://127.0.0.1:8000/signUp"
-        first_name = $("#first_name").val();
-        last_name = $("#last_name").val();
-        email = $("#email").val();
-        role = $("#type").val();
-        username = $("#username").val();
-        password = $("#password").val();
-        birth_date = $("#birth_date").val();
-        city = $("#city").val();
-        address = $("#address").val();
+        var url = "https://127.0.0.1:8000/signUp"
+        var first_name = $("#first_name").val();
+        var last_name = $("#last_name").val();
+        var email = $("#email").val();
+        var role = $("#type").val();
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var birth_date = $("#birth_date").val();
+        var city = $("#city").val();
+        var address = $("#address").val();
         //radio button of gender
-        gender=Number($('input[name=radio]:checked').val())
+        var gender = Number($('input[name=radio]:checked').val())
         sendHttpRequest("POST", url,
-         { username: username, password: password,firstname:first_name
-            ,lastname:last_name,birthdate:birth_date,gender:gender,city:city,address:address,email:email,role:role }).then(responseData => {
+            {
+                username: username,
+                password: password,
+                firstname: first_name
+                ,
+                lastname: last_name,
+                birthdate: birth_date,
+                gender: gender,
+                city: city,
+                address: address,
+                email: email,
+                role: role
+            }).then(responseData => {
             if (responseData["success"] == 'true') {
                 localStorage.setItem("usertoken", responseData["token"]);
                 location.replace("signin.html");
